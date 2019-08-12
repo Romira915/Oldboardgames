7d58f3226cc5373bfc5f140ef71015a25ff96d92
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2019-08-12 19:32:15 +0900

    init

---
+++ Oldboardgames/Source/ソース ファイル/Main.cpp
@@ -0,0 +1,20 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (ChangeWindowMode(true) != DX_CHANGESCREEN_OK || SetMainWindowText("Mancara") == -1
+		|| SetGraphMode(1280, 720, 32) == -1 || DxLib_Init() == -1
+		|| SetDrawScreen(DX_SCREEN_BACK) == -1) {
+		return -1;
+	}
+
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0)
+	{
+
+	}
+
+
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	return 0;				// ソフトの終了 
+}
