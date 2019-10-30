685fb96ce08ef05a7a108ca0f7a47db2db29f620
Yudai915 <40430090+Yudai915@users.noreply.github.com>
2019-10-30 16:18:35 +0900

    server fix

--- TCPserver/TCPserver/server.cpp
+++ TCPserver/TCPserver/server.cpp
@@ -12,6 +12,7 @@ int main()
 	int len1, len2;
 	SOCKET sock1, sock2;
 	BOOL yes = 1;
+	char buf[32];
 
 	WSAStartup(MAKEWORD(2, 0), &wsaData);
 
@@ -21,7 +22,7 @@ int main()
 	addr.sin_port = htons(59150);
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;
 
-	setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char*)& yes, sizeof(yes));
+	setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(yes));
 
 	bind(sock0, (struct sockaddr*) & addr, sizeof(addr));
 
@@ -30,15 +31,24 @@ int main()
 	while (1) {
 		len1 = sizeof(client1);
 		sock1 = accept(sock0, (struct sockaddr*) & client1, &len1);
-		send(sock1, "server", 6, 0);
-
-		closesocket(sock1);
 
 		len2 = sizeof(client2);
 		sock2 = accept(sock0, (struct sockaddr*) & client2, &len2);
 		std::string client1_ip = std::string("ip") + std::string(inet_ntoa(client1.sin_addr));
-		send(sock2, client1_ip.c_str(), client1_ip.length(), 0);
 
+		send(sock1, "server", 6, 0);
+		memset(buf, 0, sizeof(buf));
+		recv(sock1, buf, sizeof(buf), 0);
+		if (std::string(buf) == "on server")
+		{
+			send(sock2, client1_ip.c_str(), client1_ip.length(), 0);
+		}
+		else
+		{
+			send(sock2, "disconnect", 10, 0);
+		}
+
+		closesocket(sock1);
 		closesocket(sock2);
 	}
 
